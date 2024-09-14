8f28b20f39ef7353c2847a9fc3e4f71c095d9d7a
Kaique Oliveira <150181873+slkaique@users.noreply.github.com>
2024-09-14 16:55:31 -0300

    Editado o modelo de dados para o agendamentos

---
+++ agendamentos/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+class pagamento(models.model):
+    data_pagamento = models.DateField()
+    permite_recorrencia = models.BooleanField()
+    quantidade_recorrencia = models.IntegerField(validators=[MinValueValidator(0)])
+    intervalo_recorrencia = models.IntegerField(validators=[MinValueValidator(1)])
+    status_recorrencia = models.CharField(max_length=50)
+    agencia = models.IntegerField(validators=[MinValueValidator(1)])
+    conta = models.IntegerField(validators=[MinValueValidator(1)])
+    valor_pagamento = models.IntegerField(validators=[MinValueValidator(1)])
+
+    def campos(self):
+        return {
+            'id': self.id,
+            'data_pagamento': self.data_pagamento.isoformat(),
+            'permite_recorrencia': self.permite_recorrencia,
+            'quantidade_recorrencia': self.quantidade_recorrencia,
+            'intervalo_recorrencia': self.intervalo_recorrencia,
+            'status_recorrencia': self.status_recorrencia,
+            'agencia': self.agencia,
+            'conta': self.conta,
+            'valor_pagamento': self.valor_pagamento,
+        }
